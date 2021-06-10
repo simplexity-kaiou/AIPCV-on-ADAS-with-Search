ff9f1e4c826e75c9da1f4f458901370833a67e98
Frank Yin <50051786+simplexity-kaiou@users.noreply.github.com>
2021-06-10 11:10:12 +0800

    Update simplexity_NSGAII_main.py

--- code/simplexity_NSGAII_main.py
+++ code/simplexity_NSGAII_main.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
         offspring_population_size=50,
         mutation=PolynomialMutation(probability=1.0 / problem.number_of_variables, distribution_index=20),
         crossover=SBXCrossover(probability=1, distribution_index=20),
-        termination_criterion=StoppingByEvaluations(max_evaluations=7500)
+        termination_criterion=StoppingByEvaluations(max_evaluations=5000)
     )
 
     algorithm.run()
